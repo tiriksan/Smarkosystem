128dc04b5aacd032d806222df81a779728b17ec5
tiriksan <kristian.aabrekk@gmail.com>
2014-01-08 12:50:06 +0100

    BrukerEditor fix

--- Smartkosystem/src/java/spring/ov13/kontroller/BrukerEditor.java
+++ Smartkosystem/src/java/spring/ov13/kontroller/BrukerEditor.java
@@ -1,21 +1,22 @@
 package spring.ov13.kontroller;
 
 import java.beans.PropertyEditorSupport;
+import spring.ov13.domene.Bruker;
 import spring.ov13.domene.utils.UtilsBean;
-import spring.ov13.domene.Vare;
 
 
-public class VareEditor extends PropertyEditorSupport {
+
+public class BrukerEditor extends PropertyEditorSupport {
     private final UtilsBean utilsBean;
-    public VareEditor(UtilsBean ub){
+    public BrukerEditor(UtilsBean ub){
         this.utilsBean = ub;
     }
     
     @Override
     public void setAsText(String text) throws IllegalArgumentException{
         String[] t = text.split(" ");
-        Vare v = utilsBean.get(Integer.parseInt((t[0])));
-        setValue(v);   
+        Bruker b = utilsBean.get(Integer.parseInt((t[0])));
+        setValue(b);   
     }
     
 }
