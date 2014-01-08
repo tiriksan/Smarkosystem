83ccfb4104323a4ed1f3e5d8cfd6ee936d70add3
Runee <kjelaasr@gmail.com>
2014-01-08 11:48:46 +0100

    laget Plassering.java
    jau

---
+++ Smartkosystem/src/java/spring/ov13/domene/Plassering.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package spring.ov13.domene;
+
+/**
+ *
+ * @author Bruker
+ */
+public class Plassering {
+    private int bygning;
+    private int etasje;
+    private int rom;
+    private String kommentar;
+
+    public int getBygning() {
+        return bygning;
+    }
+
+    public int getEtasje() {
+        return etasje;
+    }
+
+    public int getRom() {
+        return rom;
+    }
+    
+     public String getKommentar() {
+        return kommentar;
+    }
+
+    public void setBygning(int bygning) {
+        this.bygning = bygning;
+    }
+
+    public void setEtasje(int etasje) {
+        this.etasje = etasje;
+    }
+
+    public void setRom(int rom) {
+        this.rom = rom;
+    }
+    
+     public void setKommentar(String kommentar) {
+        this.kommentar= kommentar;
+    }
+
+    @Override
+    public String toString() {
+        return "Plassering{" + "bygning=" + bygning + ", etasje=" + etasje + ", rom=" + rom + '}';
+    }
+    
+    
+    
+}
