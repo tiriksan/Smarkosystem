f4211c68694b1a18ebd39b84ff7f2af759cceadf
Charlie <haakon.jarle.hassel@gmail.com>
2014-01-09 10:42:35 +0100

    OppdaterBruker Test

--- Smartkosystem/src/java/spring/ov13/domene/utils/TestDB.java
+++ Smartkosystem/src/java/spring/ov13/domene/utils/TestDB.java
@@ -17,26 +17,15 @@ import spring.ov13.domene.Fag;
  * @author HJ
  */
 public class TestDB {
-
-    private Connection con;
-
-    @Before
-    public void getConnection() throws SQLException {
-        Properties connectionProps = new Properties();
-        connectionProps.put("user", "root");
-        connectionProps.put("password", "aXJff+6e");
-
-        con = DriverManager.getConnection("jdbc:://mysql.aitel.hist.no:3306/14-ing2-t5", connectionProps);
-
-        System.out.println("Connected to database");
-    }
+    private UtilsBean dbUtils = new UtilsBean();
+    private Database db = dbUtils.getDb();
+    private Connection con = db.getForbindelse();
     
     @Test
     public void test_registrerBruker() throws SQLException {
-        UtilsBean databaseUtils = new UtilsBean();
         Bruker b = new Bruker("luksky@hist.no", "Luke", "Skywalker", 3, "");
 
-        boolean erPersonRegistrert = databaseUtils.registrerBruker(b);
+        boolean erPersonRegistrert = dbUtils.registrerBruker(b);
         assert (erPersonRegistrert);
 
         Statement stmt = con.createStatement();
@@ -63,8 +52,33 @@ public class TestDB {
     }
     
     @Test
-    public void test_oppdaterBruker() {
-        
+    public void test_oppdaterBruker() throws SQLException {
+        Bruker b = new Bruker("luksky@hist.no", "Darth", "Vader", 3, "");
+
+        boolean erPersonOppdatert = dbUtils.registrerBruker(b);
+        assert (erPersonOppdatert);
+
+        Statement stmt = con.createStatement();
+        ResultSet res = stmt.executeQuery("SELECT * FROM bruker WHERE brukernavn='luksky@hist.no'");
+        String brukernavn = null;
+        String fornavn = null;
+        String etternavn = null;
+        int brukertype = -1;
+        String passord = null;
+
+        while (res.next()) {
+            brukernavn = res.getString("brukernavn");
+            fornavn = res.getString("fornavn");
+            etternavn = res.getString("etternavn");
+            brukertype = res.getInt("brukertype");
+            passord = res.getString("passord");
+        }
+
+        assertEquals(b.getBrukernavn(), brukernavn);
+        assertEquals(b.getFornavn(), fornavn);
+        assertEquals(b.getEtternavn(), etternavn);
+        assertEquals(b.getBrukertype(), brukertype);
+        assertEquals(b.getPassord(), passord);
     }
     
     
