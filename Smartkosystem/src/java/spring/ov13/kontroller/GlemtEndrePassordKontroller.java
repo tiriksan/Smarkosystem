022c32b215ca2b0ead98f73379ebf4298552c06e
tiriksan <kristian.aabrekk@gmail.com>
2014-01-10 11:52:19 +0100

    EndrePassord v0.9

--- Smartkosystem/src/java/spring/ov13/kontroller/GlemtEndrePassordKontroller.java
+++ Smartkosystem/src/java/spring/ov13/kontroller/GlemtEndrePassordKontroller.java
@@ -74,7 +74,7 @@ public class GlemtEndrePassordKontroller {
         //System.out.println((Bruker)request.getAttribute("endrepassordbruker"));
         
         System.out.println(bruker);
-        
+        bruker.setPassord(bruker.md5(bruker.getPassord()));
         UtilsBean ub = new UtilsBean();
         
         ub.endrePassord(bruker);
