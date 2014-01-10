94b2af201b43403231620622088149620fd05e85
Sindre <sindre.aroen@yahoo.com>
2014-01-10 09:22:56 +0100

    Div. endringer

--- Smartkosystem/src/java/spring/ov13/domene/utils/UtilsBean.java
+++ Smartkosystem/src/java/spring/ov13/domene/utils/UtilsBean.java
@@ -1,17 +1,17 @@
 package spring.ov13.domene.utils;
 
 import java.math.BigInteger;
-import java.util.List;
+import java.util.ArrayList;
 import spring.ov13.domene.Bruker;
 import spring.ov13.domene.Emne;
 
 public class UtilsBean {
     private Database db;
-    private List<Bruker> alleBrukere = null;
-    private List<Bruker> valgteBrukere = null;
-    private List<Emne> alleFag = null;
-    private List<Emne> valgteEmner = null;
-    private List<Bruker> faglærere = null;
+    private ArrayList<Bruker> alleBrukere = null;
+    private ArrayList<Bruker> valgteBrukere = null;
+    private ArrayList<Emne> alleFag = null;
+    private ArrayList<Emne> valgteEmner = null;
+    private ArrayList<Bruker> faglærere = null;
     
     
     public UtilsBean(){
@@ -27,16 +27,16 @@ public class UtilsBean {
         return db.registrerBruker(bruker);
     }
     
-    public void setValgteBrukere(List<Bruker> valgteBrukere){
+    public void setValgteBrukere(ArrayList<Bruker> valgteBrukere){
         this.valgteBrukere = valgteBrukere;
     }
     public Database getDb(){
         return db;
     }
-    public List<Bruker> getAlleBrukere(){
+    public ArrayList<Bruker> getAlleBrukere(){
         return alleBrukere;
     }
-    public List<Bruker> getValgteBrukere(){
+    public ArrayList<Bruker> getValgteBrukere(){
         return valgteBrukere;
     }
     public Bruker get(String brukernavn){
@@ -71,14 +71,14 @@ public class UtilsBean {
         return db.registrerEmne(fag);
     }
       
-      public void setValgteFag(List<Emne> valgteEmner){
+      public void setValgteFag(ArrayList<Emne> valgteEmner){
         this.valgteEmner = valgteEmner;
     }
       
-       public List<Emne> getAlleFag(){
+       public ArrayList<Emne> getAlleFag(){
         return alleFag;
     }
-    public List<Emne> getValgteEmner(){
+    public ArrayList<Emne> getValgteEmner(){
         return valgteEmner;
     }
     public Emne hent(String fagnavn){
@@ -90,7 +90,7 @@ public class UtilsBean {
         return null;
     }
     
-    public List <Bruker> getAlleFaglærere(String faglærer){
+    public ArrayList <Bruker> getAlleFaglærere(String faglærer){
         return faglærere;
     }
       
