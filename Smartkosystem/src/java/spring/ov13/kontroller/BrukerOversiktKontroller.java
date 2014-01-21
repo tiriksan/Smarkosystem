34e409c8e51e6c742a8ab8703449e1c172b64e74
Sindre <sindre.aroen@yahoo.com>
2014-01-21 10:55:39 +0100

    Brukeroversiktkontroller

--- Smartkosystem/src/java/spring/ov13/kontroller/BrukerOversiktKontroller.java
+++ Smartkosystem/src/java/spring/ov13/kontroller/BrukerOversiktKontroller.java
@@ -1,8 +1,22 @@
 package spring.ov13.kontroller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import spring.ov13.domene.Bruker;
+
 /**
  * @author Sindre
  */
+
+@SessionAttributes("brukerinnlogg")
+@Controller
 public class BrukerOversiktKontroller {
+    
+    /*@RequestMapping(value = "/brukeroversikt.htm")
+    public String visBrukerOversikt() {
+        return "brukeroversikt.htm";
+    }*/
 
 }
