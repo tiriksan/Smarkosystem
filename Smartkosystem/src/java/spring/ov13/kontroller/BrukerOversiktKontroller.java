5e24aaf2f93ca1414446ac5a2141d5f5d363e0ba
Sindre <sindre.aroen@yahoo.com>
2014-01-21 11:05:57 +0100

    BrukerOversiktKontroller.java

--- Smartkosystem/src/java/spring/ov13/kontroller/BrukerOversiktKontroller.java
+++ Smartkosystem/src/java/spring/ov13/kontroller/BrukerOversiktKontroller.java
@@ -1,10 +1,14 @@
 package spring.ov13.kontroller;
 
+import java.util.ArrayList;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import spring.ov13.domene.Bruker;
+import spring.ov13.domene.utils.UtilsBean;
 
 /**
  * @author Sindre
@@ -14,9 +18,20 @@ import spring.ov13.domene.Bruker;
 @Controller
 public class BrukerOversiktKontroller {
     
-    /*@RequestMapping(value = "/brukeroversikt.htm")
-    public String visBrukerOversikt() {
+    @RequestMapping(value = "/valgtBrukeroversikt.htm")
+    public String sendVidereTilBrukerOversikt(Model model, @RequestParam(value = "x") String emnekode){
+        
+        UtilsBean ub = new UtilsBean();
+        ArrayList<Bruker> studenter = new ArrayList<Bruker>();
+        studenter = ub.getStudenterIEmnet(emnekode);
+        model.addAttribute("studenter", studenter);
+        
         return "brukeroversikt.htm";
-    }*/
+    }
+    
+    @RequestMapping(value = "/brukeroversikt.htm")
+    public String visBrukerOversikt(Model model, @RequestParam(value = "x") String emnekode) {
+        return "brukeroversikt.htm";
+    }
 
 }
