3cbf3bec316aafde68217cec6e0be71b284f8a6e
tiriksan <kristian.aabrekk@gmail.com>
2014-01-25 00:40:25 +0100

    *Yawn* I'm not a morning person
    regArbeidskrav2.0 workin
    not yet implanted into menu

--- Smartkosystem/src/java/spring/ov13/kontroller/ArbeidskravKontroller.java
+++ Smartkosystem/src/java/spring/ov13/kontroller/ArbeidskravKontroller.java
@@ -14,36 +14,90 @@ import spring.ov13.domene.*;
 @SessionAttributes("brukerinnlogg")
 @Controller
 public class ArbeidskravKontroller {
+
     @RequestMapping(value = "/arbeidskrav.htm")
-    public String arbeidsKrav(Model modell,@ModelAttribute(value = "brukerinnlogg")Bruker bruker, HttpServletRequest request){
+    public String arbeidsKrav(Model modell, @ModelAttribute(value = "brukerinnlogg") Bruker bruker, HttpServletRequest request) {
         UtilsBean ub = new UtilsBean();
         ArrayList<Emne> emner = ub.getFageneTilBruker(bruker.getBrukernavn());
         request.getSession().setAttribute("emner", emner);
-        
+
         return "arbeidskrav";
     }
+
     @RequestMapping("/arbeidskravOving.htm")
-    public String arbeidskravØving(Model model, @RequestParam(value= "emnet")String emnekode,HttpServletRequest request){
+    public String arbeidskravØving(Model model, @RequestParam(value = "emnet") String emnekode, HttpServletRequest request) {
         System.out.println("here=");
         UtilsBean ub = new UtilsBean();
         ArrayList<Øving> øvinger = ub.getØvingerIEmnet(emnekode);
-        model.addAttribute("øvinger", øvinger);
+        request.getSession().setAttribute("øvinger", øvinger);
+        request.getSession().setAttribute("antØvinger", øvinger.size());
         request.getSession().setAttribute("emne", emnekode);
-        
-        
+
         return "arbeidskrav";
     }
-    
+
     @RequestMapping(value = "/arbeidskravVelgAnt.htm")
-    public String arbeidskravVelgAnt(Model model,HttpServletRequest request){
-        String[] valgteØvigner = request.getParameterValues("valgteØvinger");
-        model.addAttribute("valgteØvingerListe", valgteØvigner);
-        model.addAttribute("gåVidere",true);
-        System.out.println(valgteØvigner.length);
-        for (String s : valgteØvigner) {
-            System.out.println(s);
+    public String arbeidskravVelgAnt(Model model, HttpServletRequest request) {
+        String[] valgteØvinger = request.getParameterValues("valgteØvinger");
+        if (valgteØvinger == null || valgteØvinger.length < 1) {
+            return "arbeidskrav";
+        }
+        request.getSession().setAttribute("valgteØvingerListe", valgteØvinger);
+        model.addAttribute("gåVidere", true);
+        int antØvinger = (Integer) (request.getSession().getAttribute("antØvinger"));
+        boolean[] erØvingValgt = new boolean[antØvinger];
+        System.out.println(valgteØvinger.length);
+        for (String s : valgteØvinger) {
+            erØvingValgt[Integer.parseInt(s) - 1] = true;
+        }
+        model.addAttribute("erØvingValgt", erØvingValgt);
+
+        return "arbeidskrav";
+    }
+
+    @RequestMapping(value = "/registrerArbeidskrav.htm")
+    public String registrerArbeidskrav(Model model, HttpServletRequest request) {
+        String arbKravTekst = request.getParameter("arbKravText");
+        if (arbKravTekst == null || arbKravTekst.trim().equals("")) {
+            String[] valgteØvinger = (String[]) request.getSession().getAttribute("valgteØvingerListe");
+            int antØvinger = (Integer) (request.getSession().getAttribute("antØvinger"));
+            boolean[] erØvingValgt = new boolean[antØvinger];
+            for (String s : valgteØvinger) {
+                erØvingValgt[Integer.parseInt(s) - 1] = true;
+            }
+            model.addAttribute("gåVidere", true);
+            model.addAttribute("erØvingValgt", erØvingValgt);
+            model.addAttribute("tekstFeilmelding", "Må inneholde en tekstlig beskrivelse av arbeidskravet");
+        } else {
+            UtilsBean ub = new UtilsBean();
+            Kravgruppe kg = new Kravgruppe();
+            int nyGruppeID = ub.getMaxGruppeIDIEmne()+1;
+            kg.setGruppeID(nyGruppeID);
+            kg.setAntallgodkj(Integer.parseInt(request.getParameter("antØvinger")));
+            kg.setBeskrivelse(arbKravTekst);
+            kg.setEmnekode((String) request.getSession().getAttribute("emne"));
+            if (ub.registrerKravGruppe(kg)) {
+                String[] valgteØvinger = (String[]) request.getSession().getAttribute("valgteØvingerListe");
+                for (String s : valgteØvinger) {
+                    int øvingsID = Integer.parseInt(s);
+                    Øving øv = ub.getØvingIEmnet(øvingsID, (String) request.getSession().getAttribute("emne"));
+                    øv.setGruppeid(nyGruppeID);
+                    ub.oppdaterØving(øv);
+                }
+            }
+
+            model.addAttribute("RegistrertArbeidskrav", true);
         }
         return "arbeidskrav";
     }
-    
+
+    @RequestMapping(value = "/leggTilNyttArbeidskrav.htm")
+    public String leggTilNyttArbeidskrav(Model model, HttpServletRequest request) {
+        request.getSession().setAttribute("øvinger", null);
+        request.getSession().setAttribute("antØvinger", 0);
+        request.getSession().setAttribute("emne", null);
+        request.getSession().setAttribute("valgteØvingerListe", null);
+        model.addAttribute("RegistrertArbeidskrav", false);
+        return "arbeidskrav";
+    }
 }
--- Smartkosystem/web/WEB-INF/jsp/regArbeidskrav.jsp
+++ Smartkosystem/web/WEB-INF/jsp/regArbeidskrav.jsp
@@ -11,36 +11,49 @@
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 
-<c:if test="${emne==null}">
-    <p id="overEmnedrop">Velg ønsket emne!</p>
-    <form method="GET" id="emnedrop" action="arbeidskravOving.htm">
-        <select name="emnet">
-            <c:forEach items="${emner}" var="emnet">
-                <option value="${emnet.emnenavn}">${emnet.emnekode}</option>
-                <c:if test="${not empty emnekodevalgt}">
-                </c:if>
-            </c:forEach>
-        </select>
+<c:if test="${!RegistrertArbeidskrav}">
+    <h3>Legg til arbeidskrav:</h3>
+    <c:if test="${emne==null}">
+        <p id="overEmnedrop">Velg emne</p>
+        <form method="POST" id="emnedrop" action="arbeidskravOving.htm">
+            <select name="emnet">
+                <c:forEach items="${emner}" var="emnet">
+                    <option value="${emnet.emnenavn}">${emnet.emnekode}</option>
+                    <c:if test="${not empty emnekodevalgt}">
+                    </c:if>
+                </c:forEach>
+            </select>
 
-        <input type="submit" value="Velg">
-    </form>
-</c:if>
-<c:if test="${emne != null}">
-    <form action="arbeidskravVelgAnt.htm">
-        Velg øvinger:
-        <c:forEach items="${øvinger}" var="øving">
-            ${øving.øvingsnr}:<input type="checkbox" name="valgteØvinger" value="${øving.øvingsnr}">
-        </c:forEach>
-        <input type="submit" value="Gå videre">
-        here?watat
-    </form>
-    <c:if test="${gåVidere}">
-        do i even here
-        <form>
-            <input type="number" name="antØvinger" min="1" max="${fn:length(valgteØvingerListe)}">
-            <input type="text" name="arbKravText">
-            
-            <input type="submit" value="Registrer arbeidskrav">
+            <input type="submit" value="Velg">
         </form>
     </c:if>
+    <c:if test="${emne != null}">
+        <form method="POST" action="arbeidskravVelgAnt.htm">
+            Velg øvinger:
+
+            <c:forEach items="${øvinger}" var="øving">
+                ${øving.øvingsnr}:<input type="checkbox" name="valgteØvinger" value="${øving.øvingsnr}" <c:if test="${erØvingValgt != null && erØvingValgt[øving.øvingsnr-1]}">checked</c:if>>
+            </c:forEach>
+            <input type="submit" value="<c:if test="${!gåVidere}">Velg øvinger</c:if><c:if test="${gåVidere}">Oppdater øvinger</c:if>">
+            </form>
+        <c:if test="${!gåVidere}">antall øvinger og beskrivelse til arbeidskravet kommer opp etter du har valgt øvinger</c:if>
+        <c:if test="${gåVidere}">
+            <form action="registrerArbeidskrav.htm" method="GET">
+                <label for="antallØvinger">Antall øvinger:</label><input type="number" id="antallØvinger" value="1" name="antØvinger" min="1" max="${fn:length(valgteØvingerListe)}">
+                <br>
+                <label for="arbeidsKravTekst">Beskrivelse på arbeidskravet</label>
+                <input id="arbeidsKravTekst" type="text" name="arbKravText">
+                ${tekstFeilmelding}
+                <br>
+                <input type="submit" value="Registrer arbeidskrav">
+            </form>
+        </c:if>
+    </c:if>
+</c:if>
+<c:if test="${RegistrertArbeidskrav}">
+    <p>Registrering av arbeidskrav vellykket!</p>
+    <form action="leggTilNyttArbeidskrav.htm">
+        <input type="submit" value="Legg til nytt arbeidskrav">
+    </form>
+    
 </c:if>
